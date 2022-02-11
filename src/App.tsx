ff340a115b88880b146af40f5185d501a6042ccf
Hyunsuk Jo <zzoman@lunit.io>
2022-02-11 15:42:13 +0900

    Make a test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import JSZip from "jszip";
+
+// TODO : Copy and paste a zip file inside of public folder.
+const FILE_NAME = "test.zip";
+
+const fetchZip = async () => {
+  const request = new Request(`http://localhost:9000/${FILE_NAME}`, {
+    method: "GET",
+  });
+  const response = await fetch(request);
+  console.time("unzip");
+  const result = await response.arrayBuffer();
+  const unzip = await JSZip.loadAsync(result);
+  console.timeEnd("unzip");
+  console.log(unzip);
+};
 
 function App() {
+  const handleClick = async () => {
+    await fetchZip();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick} type="button">
+        Get unzip!
+      </button>
     </div>
   );
 }
